807f353816ff52ece82aa958c02c3eaea77a449f
Ryan Pals <ryanjpals@gmail.com>
2023-11-30 16:46:48 -0500

    Install redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Counter } from "./components/Counter";
 
 function App() {
-  return <div className="App">Hell world</div>;
+  return (
+    <>
+      <div className="App">Hell world</div>;
+      <Counter />;
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Counter.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { useAppSelector, useAppDispatch } from "../hooks";
+
+import { decrement, increment } from "../store/counter";
+
+export function Counter() {
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  // omit rendering logic
+  return (
+    <>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+}
