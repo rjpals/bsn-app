2587920aef69ae971b1a02e4044445f1b02c945c
Ryan Pals <ryanjpals@gmail.com>
2023-11-30 21:43:30 -0500

    Install MUI

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,11 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
